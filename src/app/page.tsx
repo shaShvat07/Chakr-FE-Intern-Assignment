707e001cb00e132e0235460e6cf2fc0c028fd784
Shashvat <shashvatpatel2803@gmail.com>
2024-07-04 06:19:48 +0530

    Minor Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl font-bold text-center text-foreground">
-        Welcome to Chakra CRM
+        Welcome to Chakr CRM
       </h1>
     </main>
   );
